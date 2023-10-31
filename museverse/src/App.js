408233e8cb2e4f1598f4fd71c10613e226303869
Mann312 <95089843+Mann202@users.noreply.github.com>
2023-10-31 15:27:12 +0700

    fix

--- museverse/src/App.js
+++ museverse/src/App.js
@@ -39,6 +39,8 @@ function App() {
                 <Route path="/artist/:artistID/discovery-all" element={<Discovery />} />
                 <Route path="/artist/:artistID/related-artists" element={<Related />} />
                 <Route path="/artist/:artistID/appear-on" element={<AppearOn />} />
+                <Route path="/album/" element={<Album />} />
+                <Route path="/album/:albumID" element={<Album />} />
                 <Route path="/track" element={<Track setPlayingTrack={setPlayingTrack} setPlayingID={setPlayingID} playingID={playingID}/>}></Route>
                 <Route path="/track/:trackID" element={<Track currentPlay={currentPlay}/>}></Route>
             </Routes>
